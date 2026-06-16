c2054698efc0808a24c3a21f7d959313e8c86cc5
Renata Azevedo <razevedo@c1r7s3.42porto.com>
2026-06-16 15:04:04 +0100

    testing the model

--- src/__main__.py
+++ src/__main__.py
@@ -1,61 +1,93 @@
+import argparse
 import json
 import numpy as np
 from pydantic import ValidationError
 from llm_sdk.llm_sdk import Small_LLM_Model
-from models import Parameter, FunctionDefinition
+from models import FunctionSchema, PromptSchema
 
 
 if __name__ == "__main__":
 
-    # WRITE CLI SKELETON THAT PRINTS - SEE ARGPARSE:
-    # - which input file was chosen
-    # - which output file was chosen
-    # - which function file was chosen
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "--functions_definition",
+            default="data/input/functions_definition.json")
+    parser.add_argument(
+            "--input", default="data/input/function_calling_tests.json")
+    parser.add_argument("--output", default="data/output/function_calls.json")
+    args = parser.parse_args()
 
+    fn_filepath = args.functions_definition
+    pr_filepath = args.input
 
+    # testing json loading and function structure
+    with open(fn_filepath, "r") as fn_file:
+        functions = json.load(fn_file)
+    functions_json = json.dumps(functions, indent=2)
 
-    # testing the model
-    """
-    prompt = "
-    You are a function calling system.
-    Available functions:
-    - fn_add_numbers(a, b)
-    - fn_greet(name)
+    for function in functions:
+        try:
+            fn_data = FunctionSchema(**function)
+        except ValidationError as e:
+            print(e)
+            raise ValidationError
 
-    User: What is the sum of 2 and 3?
-
-    Answer:
-    "
+    with open(pr_filepath, "r") as pr_file:
+        prompts = json.load(pr_file)
 
     model = Small_LLM_Model()
-    tokens = model.encode(prompt)[0].tolist()
 
-    output = []
+    for prompt in prompts:
+        try:
+            pr_data = PromptSchema(**prompt)
+        except ValidationError as e:
+            print(e)
+            raise ValidationError
 
-    for _ in range(20):
-        logits = model.get_logits_from_input_ids(tokens)
-        next_token = np.argmax(logits)
+        # testing the model
+        my_prompt = f"""
+        You are a function calling system.
+        You must choose exactly ONE function from the
+        list below and return ONLY a valid JSON object.
 
-        tokens.append(int(next_token))
-        output.append(next_token)
+        Available functions:
+        {functions_json}
+
+        Rules:
+        - Output must be valid JSON only.
+        - Do NOT write explanations.
+        - Do NOT output multiple objects.
+        - Do NOT include any extra keys like 'result'.
+        - You must select exactly one function name.
+
+        Output format:
+        {{
+            "name": "<function_name>",
+            "parameters": {{}}
+        }}
+
+        User request:
+        {pr_data.prompt}
+        """
+
+        tokens = model.encode(my_prompt)[0].tolist()
+
+        output = []
+
+        for _ in range(200):
+            logits = model.get_logits_from_input_ids(tokens)
+            next_token = np.argmax(logits)
+
+            tokens.append(int(next_token))
+            output.append(next_token)
+        text = model.decode(output)
+        print("Answer: ", text)
 
     # testing the vocabulary
-
+    """
     vocab_path = model.get_path_to_vocab_file()
     with open(vocab_path, "r") as f:
         vocab = f.read()
     parsed_vocab = json.loads(vocab)
     curly_brace_left = parsed_vocab["{"]
     """
-    # testing json loading and function structure
-
-    filepath = "data/input/functions_definition.json"
-    with open(filepath, "r") as file:
-        functions = json.load(file)
-
-    for function in functions:
-        try:
-            data = FunctionDefinition(**function)
-            print(data)
-        except ValidationError as e:
-            print(e)
--- src/models.py
+++ src/models.py
@@ -1,14 +1,17 @@
-from typing import Any, Dict
-from pydantic import BaseModel, ValidationError
+from typing import Dict
+from pydantic import BaseModel
 
 class Parameter(BaseModel):
     type: str
 
-class FunctionDefinition(BaseModel):
+class FunctionSchema(BaseModel):
     name: str
     description: str
     parameters: Dict[str, Parameter]
     returns: Dict[str, str]
+
+class PromptSchema(BaseModel):
+    prompt: str
 
 class FunctionCall(BaseModel):
     prompt: str
